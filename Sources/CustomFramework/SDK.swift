99dd4cc77588fccf426ffa8c0bb4985347704688
Diogo Gaspar <diogo.gaspar@me.com>
2022-11-14 14:28:59 -0300

    created: Framework

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Diogo Gaspar on 14/11/22.
+//
+
+import Foundation
+
+struct SDK {
+	public static func doSomeWork() {
+		print("Doing some work...")
+	}
+}
